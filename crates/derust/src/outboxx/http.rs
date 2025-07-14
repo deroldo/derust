379cbffe3c6c28284663d7c5f876e36a1b105272
Diogo Fernandes <diogofernandes@gmail.com>
2025-07-14 12:17:01 -0300

    chore: bump version to 0.3.2, add delayed message handling for HTTP, SQS, and SNS, update dependencies, and adjust README and Cargo.lock accordingly

--- crates/derust/src/outboxx/http.rs
+++ crates/derust/src/outboxx/http.rs
@@ -4,6 +4,7 @@ use outbox_pattern_processor::outbox::Outbox;
 use serde_json::Value;
 use sqlx::PgConnection;
 use std::collections::HashMap;
+use chrono::{DateTime, Utc};
 use uuid::Uuid;
 
 pub async fn send_to_http<S>(
@@ -19,5 +20,23 @@ where
     S: Clone,
 {
     let outbox = Outbox::http_post_json(partition_key, url, headers, payload);
+    insert_outbox(context, db_conn, outbox, tags).await
+}
+
+pub async fn send_to_http_with_delay<S>(
+    context: &AppContext<S>,
+    db_conn: &mut PgConnection,
+    partition_key: Uuid,
+    url: &str,
+    headers: Option<HashMap<String, String>>,
+    payload: &Value,
+    process_after: Option<DateTime<Utc>>,
+    tags: &HttpTags,
+) -> Result<Outbox, HttpError>
+where
+    S: Clone,
+{
+    let mut outbox = Outbox::http_post_json(partition_key, url, headers, payload);
+    outbox.process_after = process_after;
     insert_outbox(context, db_conn, outbox, tags).await
 }
--- crates/derust/src/outboxx/sns.rs
+++ crates/derust/src/outboxx/sns.rs
@@ -4,6 +4,7 @@ use outbox_pattern_processor::outbox::Outbox;
 use serde_json::Value;
 use sqlx::PgConnection;
 use std::collections::HashMap;
+use chrono::{DateTime, Utc};
 use uuid::Uuid;
 
 pub async fn send_to_sns<S>(
@@ -19,5 +20,23 @@ where
     S: Clone,
 {
     let outbox = Outbox::sns(partition_key, topic_arn, headers, &payload.to_string());
+    insert_outbox(context, db_conn, outbox, tags).await
+}
+
+pub async fn send_to_sns_with_delay<S>(
+    context: &AppContext<S>,
+    db_conn: &mut PgConnection,
+    partition_key: Uuid,
+    topic_arn: &str,
+    headers: Option<HashMap<String, String>>,
+    payload: &Value,
+    process_after: Option<DateTime<Utc>>,
+    tags: &HttpTags,
+) -> Result<Outbox, HttpError>
+where
+    S: Clone,
+{
+    let mut outbox = Outbox::sns(partition_key, topic_arn, headers, &payload.to_string());
+    outbox.process_after = process_after;
     insert_outbox(context, db_conn, outbox, tags).await
 }
